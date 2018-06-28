d6a12dccfe718572fc93036babfc7a709a1f4b74
Kara Siegel <c19ks@dalton.org>
2018-06-27 20:07:31 -0400

    with mongo

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template, jsonify
+from pymongo import MongoClient
 app = Flask(__name__)
 
 
-messages = [] # a pretend database
-users = []
+# messages = [] # a pretend database
+# users = []
 colors = ["purple", "red", "green", "blue", "yellow", "pink"]
 
 
@@ -14,6 +15,10 @@ def main():
 
 @app.route('/message', methods=['POST'])
 def create_message():
+    mc=MongoClient()
+    db=mc.test
+    coll=db.messages
+    collUser=db.users
     json_body = request.get_json()
     print(json_body)
     if 'text' in json_body:
@@ -21,15 +26,32 @@ def create_message():
             'message': json_body["text"],
             'user': json_body["user"]
         }
-        messages.append(message)
-        if message["user"] not in users:
-            users.append(message["user"])
+        # messages.append(message)
+        coll.insert(message)
+        # if message["user"] not in users:
+        users=list(collUser.find())
+        userStrings=[u["user"] for u in users]
+        if message["user"] not in userStrings:
+            # users.append(message["user"])
+            collUser.insert({'user': message["user"]})
     return 200
 
 
 @app.route('/getMessages', methods=['GET'])
 def get_messages():
-    json_body = {'messages': messages, "users": users, "colors": colors,}
+    mc=MongoClient()
+    db=mc.test
+    coll=db.messages
+    collUser=db.users
+    messagesList = list(coll.find())
+    for m in messagesList:
+        m["_id"] = str(m["_id"])
+    usersList = list(collUser.find())
+    for u in usersList:
+        u["_id"] = str(u["_id"])
+    # json_body = {'messages': messages, "users": users, "colors": colors,}
+    json_body = {'messages': messagesList, "users": usersList, "colors": colors,}
+    print(json_body)
     return jsonify(json_body)
 
 
